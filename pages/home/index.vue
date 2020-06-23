50782fb8f0602b4462b5902581f879661d5b5220
zhouyonghuan <2943916373>
2020-06-23 13:12:18 +0800

    初始化项目

---
+++ pages/home/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<view class="home">
+		顺风车
+		<u-button >默认按钮</u-button>
+		<u-button type="primary">主要按钮</u-button>
+		<u-button type="success">成功按钮</u-button>
+		<u-button type="info">信息按钮</u-button>
+		<u-button type="warning">警告按钮</u-button>
+		<u-button type="error">危险按钮</u-button>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ pages/mine/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<view class="mine">
+		个人中心
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
